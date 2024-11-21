2e362e7f7cb1d02554dd1593484b417cb56f7ffb
Tiburso <manuelmariamc@gmail.com>
2024-11-21 10:25:32 +0100

    feat: create config object

--- server/services/galactus/src/config.rs
+++ server/services/galactus/src/config.rs
@@ -1,8 +1,26 @@
 use dotenv::dotenv;
 
-pub fn load_env() {
+pub struct Config {
+    broker_addr: String,
+}
+
+fn load_env() {
     // Load only in development
     if cfg!(debug_assertions) {
         dotenv().ok();
+    }
+}
+
+impl Config {
+    pub fn new() -> Config {
+        load_env();
+
+        Config {
+            broker_addr: std::env::var("BROKER_ADDR").unwrap(),
+        }
+    }
+
+    pub fn broker_addr(&self) -> &str {
+        &self.broker_addr
     }
 }
--- server/services/galactus/src/main.rs
+++ server/services/galactus/src/main.rs
@@ -5,11 +5,8 @@ use brokers::create_broker_connection;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    config::load_env();
-
-    let addr = std::env::var("FASTTQ_BROKER_ADDR")?;
-
-    let broker = create_broker_connection(addr).await?;
+    let config = config::Config::new();
+    let broker = create_broker_connection(config.broker_addr().to_string()).await?;
 
     broker.register_queue("test_queue").await?;
     broker
