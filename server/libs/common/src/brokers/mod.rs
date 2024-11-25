c266b23bf6f95748107527f6285b281627ce9b46
Tiburso <manuelmariamc@gmail.com>
2024-11-25 17:11:01 +0100

    feat: enhance Broker struct with URI and queues support

--- server/libs/common/src/brokers/mod.rs
+++ server/libs/common/src/brokers/mod.rs
@@ -18,23 +18,37 @@ async fn create_broker_connection(
     }
 }
 
+#[derive(Clone)]
+pub struct Queue {
+    name: String,
+}
+
 pub struct Broker {
+    pub uri: String,
     pub broker: Box<dyn BaseBroker>,
+    pub queues: Vec<Queue>,
 }
 
 impl Broker {
     pub async fn new(uri: &String) -> Result<Self, Box<dyn std::error::Error>> {
         let broker = create_broker_connection(uri).await?;
-        Ok(Broker { broker })
+        Ok(Broker {
+            uri: uri.clone(),
+            broker,
+            queues: Vec::new(),
+        })
     }
 }
 
 impl Clone for Broker {
     fn clone(&self) -> Self {
         Broker {
+            uri: self.uri.clone(),
             broker: self.broker.clone(),
+            queues: self.queues.clone(),
         }
     }
 }
+// Needs these implementations to be able to be used on the AppState struct
 unsafe impl Send for Broker {}
 unsafe impl Sync for Broker {}
--- server/libs/common/src/models/workers.rs
+++ server/libs/common/src/models/workers.rs
@@ -7,7 +7,7 @@ use crate::models::TaskType;
 /// A worker that can execute tasks after receiving them.
 /// We know that it can receive those tasks from its list of capabilities.
 /// A worker must register itself with its capabilities to be able to receive tasks.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Worker {
     pub id: Uuid,
     pub name: String,
