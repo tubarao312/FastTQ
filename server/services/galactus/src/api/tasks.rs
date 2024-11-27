0500bc0a53184d35dded12916e53385323bd3ae3
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-11-27 01:02:57 +0000

    feat: Add task endpoints and openapi specification

--- server/services/galactus/src/api/tasks.rs
+++ server/services/galactus/src/api/tasks.rs
@@ -1,15 +1,22 @@
+use std::{alloc::System, time::SystemTime};
+
 use axum::{
     extract::{Json, Path, State},
     http::StatusCode,
     routing::{get, post, put},
     Router,
 };
-use tracing::info;
+use serde::Deserialize;
+use tracing::{error, info};
+use utoipa::ToSchema;
 use uuid::Uuid;
 
-use common::{models::Task, TaskStatus, TaskType};
+use common::{models::TaskInstance, TaskKind, TaskStatus};
 
-use crate::{repo::TaskRepository, AppState};
+use crate::{
+    repo::{TaskInstanceRepository, TaskKindRepository},
+    AppState,
+};
 
 pub fn routes() -> Router<AppState> {
     Router::new()
@@ -25,24 +32,54 @@ pub fn routes() -> Router<AppState> {
 ///
 /// # Returns
 /// Returns a JSON response containing the task if found
+#[utoipa::path(
+    get,
+    description = "Get a task by its UUID",
+    path = "/tasks/:id",
+    params(
+        ("id" = Uuid, Path, description = "Task ID to get")
+    ),
+    responses(
+        (status = 200, description = "Task found", body = TaskInstance, content_type = "application/json"),
+        (status = 404, description = "Task not found", content_type = "text/plain"),
+        (status = 500, description = "Internal server error", content_type = "text/plain")
+    ),
+    tag = "tasks"
+)]
 async fn get_task_by_id(
-    Path(id): Path<Uuid>,
     State(state): State<AppState>,
-) -> Result<Json<Task>, StatusCode> {
-    info!("Getting task by id: {:?}", id);
+    Path(id): Path<Uuid>,
+) -> Result<Json<TaskInstance>, (StatusCode, String)> {
+    info!("Getting task by ID: {:?}", id);
 
-    let task = state.task_repository.get_task_by_id(&id).await;
+    let task = state.task_repository.get_task_by_id(&id, true).await;
 
     match task {
-        Ok(task) => {
-            info!("Task with ID {:?} found: {:?}", id, task);
-            Ok(Json(task))
-        }
-        Err(e) => {
-            error!("Error getting task with ID {:?}: {:?}", id, e);
-            Err(StatusCode::NOT_FOUND)
-        }
+        Ok(task) => Ok(Json(task)),
+        Err(e) => match e {
+            sqlx::Error::RowNotFound => {
+                info!("Task with ID {:?} not found", id);
+                Err((
+                    StatusCode::NOT_FOUND,
+                    format!("Task with ID {} not found", id),
+                ))
+            }
+            _ => {
+                error!("Error getting task by id: {:?}", e);
+                Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to get task: {}", e),
+                ))
+            }
+        },
     }
+}
+
+/// Input data for creating a task
+#[derive(Debug, Deserialize, ToSchema)]
+struct CreateTaskInput {
+    task_kind_name: String,
+    input_data: Option<serde_json::Value>,
 }
 
 /// Create a new task
@@ -52,18 +89,48 @@ async fn get_task_by_id(
 ///
 /// # Returns
 /// Returns a JSON response containing the created task
+#[utoipa::path(
+    post,
+    description = "Create a new task",
+    path = "/tasks",
+    request_body = CreateTaskInput,
+    responses(
+        (status = 200, description = "Task created", body = TaskInstance, content_type = "application/json"),
+        (status = 500, description = "Internal server error", content_type = "text/plain")
+    ),
+    tag = "tasks"
+)]
 async fn create_task(
     State(state): State<AppState>,
-    Json(task): Json<Task>,
-) -> Result<Json<Task>, StatusCode> {
-    let task_type = TaskType::new("test".to_string());
+    Json(task_input): Json<CreateTaskInput>,
+) -> Result<Json<TaskInstance>, (StatusCode, String)> {
+    info!("Creating task with kind: {:?}", task_input.task_kind_name);
 
-    let task = Task::new(
-        task_type,
-        Some(serde_json::Value::String("test".to_string())),
-    );
+    // Get / Create the task kind. If it fails, return an error
+    let task_kind = state
+        .task_kind_repository
+        .get_or_create_task_kind(task_input.task_kind_name)
+        .await
+        .map_err(|e| {
+            error!("Failed to get/create task kind: {:?}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to get/create task kind: {}", e),
+            )
+        })?;
 
-    info!("Creating task: {:?}", task);
+    // Create and return the new task instance
+    let task = state
+        .task_repository
+        .create_task(task_kind.id, task_input.input_data)
+        .await
+        .map_err(|e| {
+            error!("Failed to create task: {:?}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to create task: {}", e),
+            )
+        })?;
 
     Ok(Json(task))
 }
@@ -75,10 +142,60 @@ async fn create_task(
 ///
 /// # Returns
 /// Returns a JSON response containing the updated task
+#[utoipa::path(
+    put,
+    description = "Update the status of a task. This should only be used by workers.",
+    path = "/tasks/:id/status",
+    params(
+        ("id" = Uuid, Path, description = "Task ID to update")
+    ),
+    request_body = String,
+    responses(
+        (status = 200, description = "Task status updated"),
+        (status = 400, description = "Invalid task status", content_type = "text/plain"),
+        (status = 404, description = "Task not found", content_type = "text/plain"),
+        (status = 500, description = "Internal server error", content_type = "text/plain")
+    ),
+    tag = "tasks"
+)]
 async fn update_task_status(
+    State(state): State<AppState>,
     Path(id): Path<Uuid>,
-    State(AppState { db_pools }): State<AppState>,
-) -> Result<StatusCode, StatusCode> {
+    Json(task_status): Json<String>,
+) -> Result<StatusCode, (StatusCode, String)> {
     info!("Updating task status for task: {:?}", id);
+
+    let status = TaskStatus::try_from(task_status.as_str()).map_err(|e| {
+        error!("Invalid task status: {:?}", e);
+        (
+            StatusCode::BAD_REQUEST,
+            format!("Invalid task status: {}. Valid statuses are: pending, queued, running, completed, failed, cancelled", e),
+        )
+    })?;
+
+    let task = state
+        .task_repository
+        .get_task_by_id(&id, true)
+        .await
+        .map_err(|e| {
+            error!("Task not found: {:?}", e);
+            (
+                StatusCode::NOT_FOUND,
+                format!("Task with id {} not found", id),
+            )
+        })?;
+
+    state
+        .task_repository
+        .update_task_status(&task.id, status)
+        .await
+        .map_err(|e| {
+            error!("Failed to update task status: {:?}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to update task status: {}", e),
+            )
+        })?;
+
     Ok(StatusCode::OK)
 }
